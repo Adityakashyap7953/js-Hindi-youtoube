ae6ce5b277d2caf21c7f194633cd2f2c45651eaf
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-04-01 18:04:28 +0000

    save

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+console.log(2==2);
+//true
+console.log("2" ==2 )
+//true
+console.log("2" <=2)
+//true "2" ko compiler as a number treat kr rha hai
+console.log("2" === 2)
+//false because "2" is in the form of string and 2 is in the form of number so when compaire then return false 
+console.log(null>0);
+// false
+console.log(Number(null >=0));
+//true or 1
+console.log(null==0)
+//false
