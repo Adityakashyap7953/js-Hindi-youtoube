1064c9209f5cf61a94158ea8e5c1511817d1009a
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-04-01 18:48:03 +0000

    change in

---
+++ 01_basics/datatypesummray.js
@@ -0,0 +1,42 @@
+// // premitive 
+// // 7 Types :String,Number,Boolean,Null,Undefined,BigInt,Symbol
+//    const score=100
+//    const scorevalue=100.4
+//    console.log(score)
+//    console.log(typeof(scorevalue));
+//    // both score and scorevalue variables are Number type datatype
+//    const isloggedin=false
+//    console.log(isloggedin);
+//    //false
+//    const outsidetemp=null
+//    console.log(outsidetemp)
+//    //null 
+//    let userEmail;
+//    console.log(userEmail);
+//    //undefined
+
+//    const id =Symbol('123')
+// console.log(id);
+// const anotherid=Symbol('123')
+// console.log(anotherid);
+
+// console.log(id ===anotherid);
+
+// const bignumber=345678992n
+// console.log(typeof bignumber)
+// // Refrence Type (non Primitive)
+// // Array,Objects,Function 
+const heros=new heros["shakatiman","nagara","doga"];
+console.log(typeof heros);
+
+let myobj={
+    name:"aditya",
+    age:22,
+}
+console.log(typeof myobj)
+// object
+const myFunction=function(){
+    console.log("hello word")
+}
+console.log(typeof myFunction )
+//function
