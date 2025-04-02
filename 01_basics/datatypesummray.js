54d0c1a4ef0f0e1d5367add2cbd4fa13140e85a2
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-04-02 16:13:06 +0000

    datatypesummary

--- 01_basics/datatypesummray.js
+++ 01_basics/datatypesummray.js
@@ -26,17 +26,62 @@
 // console.log(typeof bignumber)
 // // Refrence Type (non Primitive)
 // // Array,Objects,Function 
-const heros=new heros["shakatiman","nagara","doga"];
-console.log(typeof heros);
+// const heros=["shakatiman","nagara","doga"];
+// console.log(typeof heros);
+// //object
+// console.log(heros[1]);
+// nagara
 
-let myobj={
-    name:"aditya",
-    age:22,
-}
-console.log(typeof myobj)
-// object
-const myFunction=function(){
-    console.log("hello word")
-}
-console.log(typeof myFunction )
-//function
+// let myobj={
+//     name:"aditya",
+//     age:22,
+// }
+// console.log(typeof myobj)
+// // object
+// console.log(myobj.name)
+// // aditya
+// const myFunction=function(){
+//     console.log("hello word")
+// }
+// console.log(typeof myFunction )
+// //function
+// myFunction()
+// // output: hello word
+
+
+// *************************Stack(premitive) , Heap(Nonpremitive) ******************////
+// let myYoutubename="adityakashyap.com"
+// let anothername=myYoutubename
+// console.log(anothername);
+// // adityakashyap.com
+// console.log(myYoutubename);
+// // adityakashyap.com
+// anothername="kashyap@.com"
+// console.log(anothername);
+// kashyap@.com
+// In stack it gives a copy of a variable for the use so which change in the that not affect the original value only change in copy 
+
+
+// But in heap(non primitive type ) it gives a refrence of original value so that which chnage in another variable that asign the orginal value in it also change in original when change in another 
+// let myobj={
+//     gmail:"aditya@.com",
+//     upi:"aditya@ybl"
+// }
+// let anotherobj=myobj;
+// console.log(anotherobj.gmail);
+// // aditya@.com
+// anotherobj.gmail="kashyap@.com"
+// console.log(anotherobj.gmail);
+// // kashyap@.com
+// console.log(myobj.gmail);
+// // kashyap@.com
+// //  When in chnage in other then it also chnage in original 
+ function myFunction(){
+    return{
+        name:"aditya",
+    lastname:"kashyap"
+    };
+ }
+console.log(myFunction().name);
+
+ 
