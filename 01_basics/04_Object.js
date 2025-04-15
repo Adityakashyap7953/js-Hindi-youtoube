ae6d220b7b888e927eefc57198dabe15b77235ab
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-04-15 11:02:51 +0000

    04_object

--- 01_basics/04_Object.js
+++ 01_basics/04_Object.js
@@ -22,4 +22,10 @@ const regularuser={
 const obj1={1:"a",2:"b"}
 const obj2={3:"c",4:"d"}
 const obj3={obj1,obj2}
-console.log(obj3)
+// console.log(obj3)
+// console.log(obj1[2]);
+// b
+// we can also used object assign method 
+const obj4=Object.assign({},obj1,obj2,obj3)
+console.log(obj4);
+
