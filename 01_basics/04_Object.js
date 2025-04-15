1cdecec65c0b335984c65bd6d19a850bb77de7a9
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-04-15 10:13:33 +0000

    4_object

---
+++ 01_basics/04_Object.js
@@ -0,0 +1,25 @@
+const tinderuser=new Object()
+// console.log(tinderuser);
+// {}
+tinderuser.id="123abc"
+tinderuser.name="Sammy"
+tinderuser.isloggedin="false"
+// console.log(tinderuser);
+// { id: '123abc', name: 'Sammy', isloggedin: 'false' }
+const regularuser={
+    email:"some@regular",
+    fullname:{
+        userfullname:{
+            firstname:"Aditya",
+            lastname:"kashyap"
+        }
+    }
+}
+// console.log(regularuser.fullname.userfullname);
+// { firstname: 'Aditya', lastname: 'kashyap' }
+
+// value marge karna 
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3={obj1,obj2}
+console.log(obj3)
