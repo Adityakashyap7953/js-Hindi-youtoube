d9379a080c4a29114278ef5638bdbaadf710f493
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-06-09 14:52:29 +0000

    higherorder

---
+++ 01_basics/forofobject.js
@@ -0,0 +1,20 @@
+const object={
+    js:'javascript',
+    cpp:'c++',
+    rb:'rubbey',
+    swift:'swift by apple'
+}
+// for in loop is usedin obbject and als in array but in this it gives only keys 
+for(const key in object)
+{
+    // console.log(key);
+    // js
+// cpp
+// rb
+// swift
+console.log(object[key]);// this syntax objectname[key] gives the value of key 
+// javascript
+// c++
+// rubbey
+// swift by apple
+}
