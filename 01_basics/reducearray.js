b8c0ad2ddd0f4daa77f05c4f74d70f6e57a573e6
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-06-14 11:26:23 +0000

    update

--- 01_basics/reducearray.js
+++ 01_basics/reducearray.js
@@ -1,8 +1,39 @@
 const nums=[1,2,3]
-const mytotal=nums.reduce(function(acc,currval) {
-    console.log(`acc: ${acc} and currval: ${currval}`);
+// const mytotal=nums.reduce(function(acc,currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
     
-    return acc+currval;
+//     return acc+currval;
 
-}, 0)// first time accumalator value is 0 and after that in acumlator store the sum of acc+currval
-console.log(mytotal);
+// }, 0)// first time accumalator value is 0 and after that in acumlator store the sum of acc+currval
+// console.log(mytotal);
+
+
+//  const mytotal =nums.reduce((acc,currval) => acc+currval , 0)
+// console.log(mytotal);
+
+
+const Shopingcart=[
+{
+itemName:"js cource",
+price:2999
+},
+{
+    itemName:"python",
+price:999
+},
+{
+    itemName:"js cource",
+price:2999
+},
+{
+    itemName:"data cource",
+price:3999
+},
+]
+const pricetopay=Shopingcart.reduce((acc,item) => acc+item.price,0)
+
+console.log(pricetopay);
+const allname=Shopingcart.reduce((acc,item) => acc+item.itemName,0)
+console.log(allname);
+
+
