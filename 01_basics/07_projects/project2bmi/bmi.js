69ee8aac7478b6ceda5149fc709ff42628037e10
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-07-06 13:51:21 +0000

    bmi

---
+++ 01_basics/07_projects/project2bmi/bmi.js
@@ -0,0 +1,16 @@
+const form=document.querySelector('form');
+form.addEventListener('submit',function(e)
+{
+e.preventDefault();
+const height=parseInt(document.querySelector('#height').value);
+const weight=parseInt(document.querySelector('#weight').value);
+const results=document.querySelector('#results');
+if(height==='' || height<0 || isNaN(height)){
+results.innerHTML="Please enetr a valid height ";
+}
+// results.innerHTML=${'height'}
+// if(weight=='' || weight<0 || isNaN(weight)){
+// results.innerHTML=`Please enetr a valid weight`;
+// }
+    
+});
