b05258465c527a1204b6add2861e85b06518d70e
Adityakashyap7953 <132939328+Adityakashyap7953@users.noreply.github.com>
2025-07-06 14:22:50 +0000

    commit

--- 01_basics/07_projects/project2bmi/bmi.js
+++ 01_basics/07_projects/project2bmi/bmi.js
@@ -1,16 +1,36 @@
-const form=document.querySelector('form');
-form.addEventListener('submit',function(e)
-{
-e.preventDefault();
-const height=parseInt(document.querySelector('#height').value);
-const weight=parseInt(document.querySelector('#weight').value);
-const results=document.querySelector('#results');
-if(height==='' || height<0 || isNaN(height)){
-results.innerHTML="Please enetr a valid height ";
-}
-// results.innerHTML=${'height'}
-// if(weight=='' || weight<0 || isNaN(weight)){
-// results.innerHTML=`Please enetr a valid weight`;
-// }
-    
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height < 0 || isNaN(height)) {
+    results.innerHTML = "Please enter a valid height";
+    return;
+  }
+
+  if (weight < 0 || isNaN(weight)) {
+    results.innerHTML = "Please enter a valid weight";
+    return;
+  }
+
+  const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+//   results.innerHTML = `Your BMI is ${bmi}`;
+
+
+
+   let category = '';
+  if (bmi < 18.6) {
+    category = "Underweight";
+  } else if (bmi >= 18.6 && bmi <= 24.9) {
+    category = "Normal weight";
+  } else {
+    category = "Overweight";
+  }
+
+  // Show result with category
+  results.innerHTML = `Your BMI is <strong>${bmi}</strong><br>Category: <strong>${category}</strong>`;
 });
